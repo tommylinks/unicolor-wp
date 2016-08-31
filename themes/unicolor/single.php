1a889a447f0cb12974023fc539a871bdb71d2083
tommylinks <tommylinks1@gmail.com>
2016-08-31 08:05:31 +0300

    add disqus

--- themes/unicolor/single.php
+++ themes/unicolor/single.php
@@ -24,9 +24,9 @@ get_header(); ?>
                 <a href="/" class="btn snt-back-home">На главную</a>
               </div>
               
-              <div class="container">
+              
                <div class="same-articles"> 
-                 <h3>Похожие записи</h3>
+                 
                   <div class="row">
                        <?php
                         $orig_post = $post;
@@ -64,9 +64,31 @@ get_header(); ?>
                         ?>
                         </div> 
                       </div>
-                     </div>
+                     
                     
-
+                <div class="snt-disqus">
+                  <div id="disqus_thread"></div>
+                    <script>
+                  
+                    /**
+                     *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+                     *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables */
+                    /*
+                    var disqus_config = function () {
+                        this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+                        this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+                    };
+                    */
+                    (function() { // DON'T EDIT BELOW THIS LINE
+                        var d = document, s = d.createElement('script');
+                        s.src = '//http-unicolor-com-ua-2.disqus.com/embed.js';
+                        s.setAttribute('data-timestamp', +new Date());
+                        (d.head || d.body).appendChild(s);
+                    })();
+                    </script>
+                    <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+                </div>
+                                    
               
             </div>
           </div>
