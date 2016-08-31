40af3a2bab8105dd8a3874332164872f1272e526
tommylinks <tommylinks1@gmail.com>
2016-08-31 07:52:34 +0300

    add component - same posts

--- themes/unicolor/single.php
+++ themes/unicolor/single.php
@@ -23,6 +23,51 @@ get_header(); ?>
               <div class="text-center">
                 <a href="/" class="btn snt-back-home">На главную</a>
               </div>
+              
+              <div class="container">
+               <div class="same-articles"> 
+                 <h3>Похожие записи</h3>
+                  <div class="row">
+                       <?php
+                        $orig_post = $post;
+                        global $post;
+                        $tags = wp_get_post_tags($post->ID);
+                    
+                        if ($tags) {
+                        $tag_ids = array();
+                        foreach($tags as $individual_tag) $tag_ids[] = $individual_tag->term_id;
+                        $args=array(
+                        'tag__in' => $tag_ids,
+                        'post__not_in' => array($post->ID),
+                        'posts_per_page'=>3, // Количество записей, которые выводятся
+                        'caller_get_posts'=>1
+                        );
+                    
+                        $my_query = new wp_query( $args );
+                    
+                        while( $my_query->have_posts() ) {
+                        $my_query->the_post();
+                        ?>
+                        
+                            <div class="col-md-4 col-sm-4 col-xs-12">
+                                <div class="relatedthumb">
+                                    <a rel="external" href="<? the_permalink()?>"><?php the_post_thumbnail('medium', 'class=img-responsive'); ?><br />
+                                    <?php the_title(); ?>
+                                    </a>
+                                </div>
+                             </div>
+                            
+                        <? }
+                        }
+                        $post = $orig_post;
+                        wp_reset_query();
+                        ?>
+                        </div> 
+                      </div>
+                     </div>
+                    
+
+              
             </div>
           </div>
         </div>   
